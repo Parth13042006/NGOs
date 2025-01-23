cfc3dc843d00bb0c5c15e059d988334d6bbdf8cc
Parth13042006 <ptyagi1444@gmail.com>
2025-01-23 15:49:02 +0530

    Add files via upload

---
+++ New folder (5)/script.js
@@ -0,0 +1,19 @@
+
+    /*<script>
+        // Function to handle donation form submission
+        document.getElementById("donationForm").addEventListener("submit", function(event)) {
+            event.preventDefault(); // Prevent form from submitting
+
+            // Get values from the form
+            var organization = document.getElementById("organization").value;
+            var amount = document.getElementById("amount").value;
+
+            // Hide donation form and show confirmation
+            document.getElementById("donation-section").style.display = "none";
+            document.getElementById("donation-confirmation").style.display = "block";
+
+            // Display donation details
+            var message = "You have donated " + amount + " INR to " + organization + ".";
+            document.getElementById("donation-message").innerText = message;
+       
+    </script>*/
